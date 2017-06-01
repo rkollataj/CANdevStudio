84164a67c649a35b2ca0bd3fd85bdc3cb214a4fe
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-06-01 14:44:05 +0200

    Another test
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,7 +1,11 @@
 #define CATCH_CONFIG_RUNNER
 #include <fakeit.hpp>
+#include "candevice/candevice.h"
 
 TEST_CASE("Simple test", "[example]") {
+    CanDevice cd;
+    cd.init("peakcan", "can0");
+    cd.start();
 }
 
 int main(int argc, char *argv[])
