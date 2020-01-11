dc4e5d26be8eb1c7975fda24599591041e07af7f
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2020-01-11 10:39:38 +0100

    fix filename clash with Python context.h
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- src/components/pyscripter/pyscripter.h
+++ src/components/pyscripter/pyscripter.h
@@ -4,7 +4,7 @@
 #include <QWidget>
 #include <QtCore/QScopedPointer>
 #include <componentinterface.h>
-#include <context.h>
+#include <common/context.h>
 #include <memory>
 
 class PyScripterPrivate;
